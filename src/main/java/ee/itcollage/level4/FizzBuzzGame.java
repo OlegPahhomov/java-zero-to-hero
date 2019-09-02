0da05118db59ad6b7601ebafa90dbadd53e40aa8
oleg.pahhomov <oleg.pahhomov@gmail.com>
2019-09-02 10:45:57 +0300

    Solution for l4

--- src/main/java/ee/itcollage/level4/FizzBuzzGame.java
+++ src/main/java/ee/itcollage/level4/FizzBuzzGame.java
@@ -19,10 +19,67 @@ public class FizzBuzzGame {
 
         System.out.println("Example 1");
         for (Integer number : numbers) {
-            //do your logic
-            System.out.print(number);
+            if (number % 3 == 0 && number % 5 == 0) {
+                System.out.print("FizzBuzz");
+            } else if (number % 3 == 0) {
+                System.out.print("Fizz");
+            } else if (number % 5 == 0) {
+                System.out.print("Buzz");
+            } else {
+                System.out.print(number);
+            }
             System.out.print(" ");
         }
         System.out.println();
+
+        System.out.println("Example 2");
+        for (Integer number : numbers) {
+            String string = "";
+            if (number % 3 == 0) string += "Fizz";
+            if (number % 5 == 0) string += "Buzz";
+            if (string.length() == 0) string += number;
+            System.out.print(string);
+            System.out.print(" ");
+        }
+        System.out.println();
+
+        System.out.println("Example 3");
+        for (Integer number : numbers) {
+            String value;
+            switch (number % 15) {
+                case 3:
+                case 6:
+                case 9:
+                case 12:  // divisible by 3, print Fizz
+                    value = "Fizz";
+                    break;
+                case 5:
+                case 10:  // divisible by 5, print Buzz
+                    value = "Buzz";
+                    break;
+                case 0:  // divisible by 3 and by 5, print FizzBuzz
+                    value = "FizzBuzz";
+                    break;
+                default:  // else print the number
+                    value = Integer.toString(number);
+            }
+            System.out.print(value);
+            System.out.print(" ");
+        }
+        System.out.println();
+
+        System.out.println("Example 4");
+        for (Integer number : numbers) {
+            String string = Optional.of(number)
+                    .map(n -> (n % 3 == 0 ? "Fizz" : "") + (n % 5 == 0 ? "Buzz" : ""))
+                    .get();
+            String result = !string.isEmpty() ? string : Integer.toString(number);
+            System.out.print(result);
+            System.out.print(" ");
+        }
+        System.out.println();
+
+        // enterprised edition
+        // https://github.com/up1/kata_fizzbuzz_ocp
     }
 }
