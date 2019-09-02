9d002271e3769cb8be31042c74f9e88a3b0d344a
oleg.pahhomov <oleg.pahhomov@gmail.com>
2019-09-02 19:57:30 +0300

    Solution for l10

--- src/main/java/ee/itcollage/level10/Cardgame.java
+++ src/main/java/ee/itcollage/level10/Cardgame.java
@@ -3,11 +3,18 @@ package ee.itcollage.level10;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Cardgame {
 
     public static List<Card> buildDeck() {
         List<Card> cards = new ArrayList<>();
+        for (Rank rank : Rank.values()) {
+            for (Suit suit : Suit.values()) {
+                Card card = new Card(rank, suit);
+                cards.add(card);
+            }
+        }
         return cards;
     }
 
@@ -18,6 +25,10 @@ public class Cardgame {
 
     public static void main(String[] args) {
         //todo 1 fix tests
+        List<Card> cards = buildDeck();
+        shuffle(cards);
+        List<Card> pokerHand = cards.stream().limit(5).collect(Collectors.toList());
+        System.out.println(pokerHand);
         //todo 2 simulate playing poker (buildDeck, shuffle and get 5 cards)
 
     }
