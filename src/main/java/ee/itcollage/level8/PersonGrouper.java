ca84dadeac4b6742a73a8dfd4ea3a08ecc61893d
oleg.pahhomov <oleg.pahhomov@gmail.com>
2019-09-02 11:07:57 +0300

    Solution for l7, l8, l9

--- src/main/java/ee/itcollage/level8/PersonGrouper.java
+++ src/main/java/ee/itcollage/level8/PersonGrouper.java
@@ -3,12 +3,17 @@ package ee.itcollage.level8;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class PersonGrouper {
 
     //todo fix tests
     public static Map<String, Person> groupPeopleByIdCode(List<Person> people){
-        return new HashMap<>();
+        Map<String, Person> map = new HashMap<>();
+        for (Person person : people) {
+           map.put(person.getIdCode(), person);
+        }
+        return map;
     }
 
 }
--- src/main/java/ee/itcollage/level9/StringMaker.java
+++ src/main/java/ee/itcollage/level9/StringMaker.java
@@ -9,15 +9,17 @@ public class StringMaker {
      * @param x how many times you want the string
      */
     public static String make(String s, int x) {
-        String chars = "";
-        return chars;
+        String result = "";
+        for (int i = 0; i < x; i++) {
+            result += s;
+        }
+        return result;
     }
 
     /**
      * Gives you a text containing of x times of string c plus y times of string c.
      */
     public static String make(String s1, int x1, String s2, int x2) {
-        String chars = "";
-        return chars;
+        return s1.repeat(Math.max(x1, 0)) + s2.repeat(Math.max(x2, 0));
     }
 }
