1627ae28fff21a02717b6888fbf51bceb9e1ab5b
oleg.pahhomov <oleg.pahhomov@gmail.com>
2019-09-09 10:42:40 +0300

    Palindrome solution

--- src/main/java/ee/itcollage/level13/AnalysisResult.java
+++ src/main/java/ee/itcollage/level13/AnalysisResult.java
@@ -6,4 +6,25 @@ public class AnalysisResult {
 
     private List<String> palidromes;
     private List<String> others;
+
+    public List<String> getPalidromes() {
+        return palidromes;
+    }
+
+    public void setPalidromes(List<String> palidromes) {
+        this.palidromes = palidromes;
+    }
+
+    public List<String> getOthers() {
+        return others;
+    }
+
+    public void setOthers(List<String> others) {
+        this.others = others;
+    }
+
+    public AnalysisResult(List<String> palidromes, List<String> others) {
+        this.palidromes = palidromes;
+        this.others = others;
+    }
 }
--- src/main/java/ee/itcollage/level13/PalindromeWords.java
+++ src/main/java/ee/itcollage/level13/PalindromeWords.java
@@ -1,6 +1,9 @@
 package ee.itcollage.level13;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class PalindromeWords {
 
@@ -13,17 +16,36 @@ public class PalindromeWords {
      * For example: radar (reversed radar) is a palindrome
      * cow (reversed woc) is not a palindrome
      */
-    public AnalysisResult analyzePalindromes(List<String> strings){
-        return new AnalysisResult();
+    public AnalysisResult analyzePalindromes(List<String> strings) {
+        List<String> palindrome = new ArrayList<>();
+        List<String> otherWords = new ArrayList<>();
+        for (String string : strings) {
+            if (string.equals(reversedString(string))) {
+                palindrome.add(string);
+            } else {
+                palindrome.add(string);
+            }
+        }
+
+        //v2
+        Map<Boolean, List<String>> collect = strings.stream()
+                .collect(Collectors.partitioningBy(string -> string.equals(reversedString(string))));
+        new AnalysisResult(collect.get(true), collect.get(false));
+
+        return new AnalysisResult(palindrome, otherWords);
     }
 
     public static void main(String[] args) {
         String palidrome = "radar";
-        String reversedPalidrome = new StringBuilder(palidrome).reverse().toString();
+        String reversedPalidrome = reversedString(palidrome);
         String other = "other";
-        String otherReversed = new StringBuilder(other).reverse().toString();
+        String otherReversed = reversedString(other);
 
         System.out.println(String.format("%s %s", palidrome, reversedPalidrome));
         System.out.println(String.format("%s %s", other, otherReversed));
+    }
+
+    private static String reversedString(String string) {
+        return new StringBuilder(string).reverse().toString();
     }
 }
