7492b74245a48685ddaed908a682edf7cfea9695
Oleg Pahhomov <oleg.pahhomov@gmail.com>
2020-02-13 10:06:34 +0200

    Update suit

--- src/main/java/ee/itcollage/level10/Suit.java
+++ src/main/java/ee/itcollage/level10/Suit.java
@@ -14,22 +14,22 @@ public enum Suit {
 	public String toString() {
 		char icon;
 		switch (this) {
-		case CLUBS:
-			icon = '♣';
-			break;
-		case DIAMONDS:
-			icon = '♦';
-			break;
-		case HEARTS:
-			icon = '♥';
-			break;
-		case SPADES:
-			icon = '♠';
-			break;
-		default:
-			throw new IllegalStateException("No icon implemented for " + super.toString());
+			case CLUBS:
+				icon = '\u2663';
+				break;
+			case DIAMONDS:
+				icon = '\u2666';
+				break;
+			case HEARTS:
+				icon = '\u2665';
+				break;
+			case SPADES:
+				icon = '\u2660';
+				break;
+			default:
+				throw new IllegalStateException("No icon implemented for " + super.toString());
 		}
-		
+
 		return String.valueOf(icon);
 	}
 }
