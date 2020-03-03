3319b2b70ab35d4f35b1b5135cc29532035f1e97
Oleg Pahhomov <oleg.pahhomov@gmail.com>
2020-03-03 10:28:39 +0200

    Add a solution for duplicate remover

--- src/main/java/ee/itcollage/level7/DuplicateRemover.java
+++ src/main/java/ee/itcollage/level7/DuplicateRemover.java
@@ -1,11 +1,14 @@
 package ee.itcollage.level7;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 public class DuplicateRemover {
 
     //todo fix tests
     public static List<Integer> removeDuplicates(List<Integer> numbers){
-        return numbers;
+        HashSet<Integer> integers = new HashSet<>(numbers);
+        return new ArrayList<>(integers);
     }
 }
