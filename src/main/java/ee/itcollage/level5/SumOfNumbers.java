7f4fc223cd378db42e1c0986a5a90d29fe5bce3f
Oleg Pahhomov <oleg.pahhomov@gmail.com>
2020-03-03 09:28:19 +0200

    Add a collectionutils dependency for sum of numbers

--- src/main/java/ee/itcollage/level5/SumOfNumbers.java
+++ src/main/java/ee/itcollage/level5/SumOfNumbers.java
@@ -1,12 +1,18 @@
 package ee.itcollage.level5;
 
+import org.apache.commons.collections4.CollectionUtils;
+
 import java.util.List;
+
+import static org.apache.commons.collections4.CollectionUtils.isEmpty;
+import static org.apache.commons.collections4.CollectionUtils.isNotEmpty;
 
 public class SumOfNumbers {
 
     //todo make tests work
     public static Integer sum(List<Integer> integerList) {
-        if (integerList == null || integerList.isEmpty()) {
+        if (isEmpty(integerList)){
+//        if (integerList == null || integerList.isEmpty()) {
             //todo add gradle dependency for apache collectionutils and rewrite this condition with isNotEmpty
             return null;
         }
