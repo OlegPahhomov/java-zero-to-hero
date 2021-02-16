f9a5c08da6e135fcc03607503fe975c4ffa734e0
Oleg Pahhomov <oleg.pahhomov@gmail.com>
2021-02-16 09:06:31 +0200

    level2 solution

--- src/main/java/ee/itcollage/level2/Multiplication.java
+++ src/main/java/ee/itcollage/level2/Multiplication.java
@@ -5,5 +5,11 @@ public class Multiplication {
     public static void main(String[] args) {
         //todo create function in this class that multiplies 2 inputs
         // call the function in the main method, for example with 5 and 4: 5x4 should be 20
+        int result = multiply(5, 4);
+        System.out.println(result);
+    }
+
+    public static int multiply(int x1, int x2){
+        return x1 * x2;
     }
 }
