35a33b3416f2cbaae4cd3b4bfa2afa0ff44661fa
Oleg Pahhomov <oleg.pahhomov@gmail.com>
2021-02-16 09:14:49 +0200

    level3 solution

--- src/main/java/ee/itcollage/level3/LargestNumber.java
+++ src/main/java/ee/itcollage/level3/LargestNumber.java
@@ -1,14 +1,17 @@
 package ee.itcollage.level3;
 
+import java.util.stream.IntStream;
+
 public class LargestNumber {
 
     //todo make tests work
 
-    public static int pickLargest(int x1, int x2){
-        return x1;
+    public static int pickLargest(int x1, int x2) {
+        return Math.max(x1, x2);
     }
 
-    public static int pickLargest(int x1, int x2, int x3){
-        return x1;
+    public static int pickLargest(int x1, int x2, int x3) {
+//        return IntStream.of(x1, x2, x3).max().getAsInt();
+        return Math.max(Math.max(x1, x2), x3);
     }
 }
