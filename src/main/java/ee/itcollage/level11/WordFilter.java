c3541828c4d3c65e7c0098bad0b490e421795d91
Oleg Pahhomov <oleg.pahhomov@gmail.com>
2021-02-16 09:35:15 +0200

    level11 solution

--- src/main/java/ee/itcollage/level11/WordFilter.java
+++ src/main/java/ee/itcollage/level11/WordFilter.java
@@ -2,36 +2,58 @@ package ee.itcollage.level11;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class WordFilter {
 
     //todo fix tests
 
     public List<Word> getNouns(List<Word> words) {
-        return words;
+        return words.stream()
+                .filter(word -> word.getWordType().equals(Word.WordType.NOUN))
+                .collect(Collectors.toList());
     }
 
     public Word getFirstVerb(List<Word> words) {
-        return words.get(0);
+        return words.stream()
+                .filter(word -> word.getWordType().equals(Word.WordType.VERB))
+                .findFirst()
+                .orElse(null);
     }
 
     public Word getFirstOfType(List<Word> words, Word.WordType type) {
-        return words.get(0);
+        return words.stream()
+                .filter(word -> word.getWordType().equals(type))
+                .findFirst()
+                .orElse(null);
     }
 
     public List<Word> getAllNotNouns(List<Word> words) {
-        return words;
+        return words.stream()
+                .filter(word -> !word.getWordType().equals(Word.WordType.NOUN))
+                .collect(Collectors.toList());
     }
 
     public List<String> getNounStrings(List<Word> words){
-        return new ArrayList<>();
+        return words.stream()
+                .filter(word -> word.getWordType().equals(Word.WordType.NOUN))
+                .map(Word::getWord)
+                .collect(Collectors.toList());
     }
 
     public String getFirstVerbString(List<Word> words) {
-        return "";
+        return words.stream()
+                .filter(word -> word.getWordType().equals(Word.WordType.VERB))
+                .map(Word::getWord)
+                .findFirst()
+                .orElse(null);
     }
 
     public String getFirstStringOfType(List<Word> words, Word.WordType type) {
-        return "";
+        return words.stream()
+                .filter(word -> word.getWordType().equals(type))
+                .map(Word::getWord)
+                .findFirst()
+                .orElse(null);
     }
 }
