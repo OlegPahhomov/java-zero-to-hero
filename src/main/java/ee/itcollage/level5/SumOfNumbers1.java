8e2de83b0145338731461320a687f4b2e8c9218a
Oleg Pahhomov <oleg.pahhomov@gmail.com>
2022-02-15 10:55:13 +0200

    pseudocode

--- src/main/java/ee/itcollage/level5/SumOfNumbers1.java
+++ src/main/java/ee/itcollage/level5/SumOfNumbers1.java
@@ -5,7 +5,7 @@ public class SumOfNumbers1 {
     //todo write here pseudo-code how to find sum of numbers
     // you have List<Integer> input, what do you do?
     //
-    //
-    //
-    //
+    //     sum = 0
+    //     for item in list
+    //     sum = sum + item
 }
